43415e573caef01336c3d1310b7402d42fd06155
Dan Bartlett <danbartlett@gmail.com>
2012-10-30 21:58:05 +0000

    Added some js to toggle the display of title and duration fields dependent on sit type.

--- app/assets/javascripts/sits.js
+++ app/assets/javascripts/sits.js
@@ -3,4 +3,16 @@ jQuery(document).ready(function($) {
     window.location=$(this).find("a.sit-link").attr("href"); 
     return false;
   })
+
+  $('.radio_buttons').click(function(){
+    if ($(this).attr("id") == "sit_s_type_0")
+    {
+      $('.new-sit-title').hide();
+      $('.new-sit-duration').show();
+    } else {
+      $(".new-sit-title").show();
+      $('.new-sit-duration').hide();
+    }
+	    
+	});
 });
