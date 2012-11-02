b7caa64cf1af02025f202ce49ce24d23f62dc411
Dan Bartlett <danbartlett@gmail.com>
2012-11-02 08:53:10 +0000

    Removed the line that was including all the bootstrap js files & turned images off in the editor

--- app/assets/javascripts/main.js
+++ app/assets/javascripts/main.js
@@ -6,7 +6,7 @@ $(document).ready(function(){
       "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
       "html": false, //Button which allows you to edit the generated HTML. Default false
       "link": true, //Button to insert a link. Default true
-      "image": true, //Button to insert an image. Default true,
+      "image": false, //Button to insert an image. Default true,
       "color": false //Button to change color of font
     });
   });
