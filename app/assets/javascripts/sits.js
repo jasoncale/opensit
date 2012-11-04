19d391284f417ad12975fcf53e469dda996b8c1c
Thomas Buckley-Houston <tom@tombh.co.uk>
2012-11-04 20:29:38 +0000

    JS for toggling duration selector on new sit. Fixes #14

--- app/assets/javascripts/sits.js
+++ app/assets/javascripts/sits.js
@@ -1,18 +1,19 @@
 jQuery(document).ready(function($) {
   // $('.sit-teaser').click(function() {
-  //   window.location=$(this).find("a.sit-link").attr("href"); 
+  //   window.location=$(this).find("a.sit-link").attr("href");
   //   return false;
   // })
 
-  $('.radio_buttons').click(function(){
-    if ($(this).attr("id") == "sit_s_type_0")
-    {
+  $('.new-sit .radio_buttons input').click(function(){
+    // If the 'sit' button was pressed
+    if($(this).attr('id') == 'sit_s_type_0'){
+      // Hide the title and show the duration selector
       $('.new-sit-title').hide();
       $('.new-sit-duration').show();
-    } else {
+    }else{
       $(".new-sit-title").show();
       $('.new-sit-duration').hide();
     }
-	    
-	});
+  });
+
 });
