96dcbec690c8b15c49f3fe2f26edccdcfffd3896
Dan Bartlett <danbartlett@gmail.com>
2013-01-12 15:53:53 +0000

    Schema update

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130112114703) do
+ActiveRecord::Schema.define(:version => 20121103125501) do
 
   create_table "comments", :force => true do |t|
     t.text     "body"
@@ -50,11 +50,10 @@ ActiveRecord::Schema.define(:version => 20130112114703) do
     t.text     "body"
     t.integer  "user_id"
     t.boolean  "disable_comments"
-    t.datetime "created_at",       :null => false
-    t.datetime "updated_at",       :null => false
+    t.datetime "created_at"
+    t.datetime "updated_at"
     t.integer  "duration"
     t.integer  "s_type"
-    t.date     "custom_date"
   end
 
   create_table "users", :force => true do |t|
