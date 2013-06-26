10d0745d219051f5a1317e292541fb6014b9584e
Dan Bartlett <danbartlett@gmail.com>
2013-06-26 19:53:31 +0100

    No access for guests [fixes #52184201]

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,10 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
+  before_filter :custom_user_auth, only: [:front]
+
+	def custom_user_auth
+    unless user_signed_in?
+    	redirect_to('/users/sign_in')
+    end
+	end
 end
