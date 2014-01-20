84df4ddca52066d104c1bacf07a87d673507c7ea
John Kelly Ferguson <hello@johnkellyferguson.com>
2014-01-20 03:15:14 -0500

    Add traits to Sits factory

--- spec/factories/sits.rb
+++ spec/factories/sits.rb
@@ -1,9 +1,42 @@
 FactoryGirl.define do
   factory :sit do
-    user
     body "I done a meditate."
     s_type 0
     duration 30
+
+    trait :journal do
+      s_type 1
+      title "Making factories"
+      duration nil
+    end
+
+    trait :belongs_to_user do
+      association :user, factory: :user
+    end
+
+    trait :private do
+      private true
+    end
+
+    trait :public do
+      private false
+    end
+
+    trait :one_day_ago do
+      created_at { 1.days.ago }
+    end
+
+    trait :two_days_ago do
+      created_at { 2.days.ago }
+    end
+
+    trait :three_days_ago do
+      created_at { 3.days.ago }
+    end
+
+    trait :four_days_ago do
+      created_at { 4.days.ago }
+    end
   end
 end
 
