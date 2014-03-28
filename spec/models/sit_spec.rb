8b459dc94dd7217c13462f5c98c67c900aaff3ca
Dan Bartlett <danbartlett@gmail.com>
2014-03-28 19:58:02 +0000

    Tests for incrementing/resetting sit streaks.

---
+++ spec/models/sit_spec.rb
@@ -0,0 +1,25 @@
+require 'spec_helper'
+
+describe Sit do
+  let(:buddha) { create(:buddha) }
+
+	describe "streaks" do
+  	context "sat yesterday" do
+	  	it "increments streak by 1" do
+	  		expect(buddha.streak).to eq 1
+	  		last_sit = create(:sit, created_at: Date.today - 1)
+	  		todays = create(:sit, created_at: Date.today)
+	  		expect(buddha.streak).to eq 2
+	  	end
+	  end
+
+	  context "missed a day" do
+	  	it "resets streak" do
+	  		buddha.streak = 16
+	  		last_sit = create(:sit, created_at: Date.today - 2)
+	  		todays = create(:sit, created_at: Date.today)
+	  		expect(buddha.streak).to eq 1
+	  	end
+	  end
+  end
+end
