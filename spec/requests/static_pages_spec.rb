de8332db5b936ad37044fb08aa9959b3b56229f9
Dan Bartlett <danbartlett@gmail.com>
2014-04-06 00:53:40 +0100

    Added integration tests for other static pages, peace of mind.

--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -6,13 +6,21 @@ describe "Static pages" do
 
   describe "Home page" do
     before { visit root_path }
-
     it { should have_selector('h1',    text: 'Where meditators meet.') }
   end
 
   describe "About page" do
     before { visit about_path }
-
     it { should have_selector('h1',    text: 'About') }
+  end
+
+  describe "Contact page" do
+    before { visit contact_path }
+    it { should have_selector('h1',    text: 'Contact us') }
+  end
+
+  describe "Contribute page" do
+    before { visit contribute_path }
+    it { should have_selector('h1',    text: 'Help us build OpenSit!') }
   end
 end
