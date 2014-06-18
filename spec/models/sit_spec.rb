890ce8c773897325dff63b45b2ce1d9636e7d941
Dan Bartlett <danbartlett@gmail.com>
2014-06-18 21:10:45 +0100

    Stop abusing test db!

--- spec/models/sit_spec.rb
+++ spec/models/sit_spec.rb
@@ -22,21 +22,21 @@ describe Sit do
 	  	end
 
 	  	it "doesn't increment if already sat today" do
-        7.times do |i|
+        2.times do |i|
           create(:sit, user: buddha, created_at: Date.yesterday - i)
         end
         # Morning sit
         create(:sit, user: buddha, created_at: Date.today + 9.hours)
-        expect(buddha.reload.streak).to eq 8
+        expect(buddha.reload.streak).to eq 3
         # Evening sit
 	  	  create(:sit, user: buddha, created_at: Date.today + 19.hours)
-	  		expect(buddha.reload.streak).to eq 8
+	  		expect(buddha.reload.streak).to eq 3
 	  	end
 	  end
 
 	  context "missed a day" do
 	  	it "resets streak" do
-	  		16.times do |i|
+	  		2.times do |i|
           create(:sit, user: buddha, created_at: Date.yesterday - (i + 1))
         end
 	  		create(:sit, user: buddha, created_at: Date.today)
