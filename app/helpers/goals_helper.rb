b427259d31d882d0fe8cb819d9a95874d09cf44b
Dan Bartlett <danbartlett@gmail.com>
2014-07-24 21:24:12 +0100

    days_ago_in_words, different wording for completed goal partial

--- app/helpers/goals_helper.rb
+++ app/helpers/goals_helper.rb
@@ -11,4 +11,11 @@ module GoalsHelper
       "<span class='label label-warning'>#{goal.rating}%</span>".html_safe
     end
   end
+
+  def days_ago_in_words(from)
+  	days = (Date.today - from.to_date).to_i
+  	return 'today' if days == 0
+  	return 'yesterday' if days == 1
+  	return "#{days} days ago"
+  end
 end
