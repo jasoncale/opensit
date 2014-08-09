7515e4b27273c8bc016a8e45b18f31a2f3636053
Dan Bartlett <danbartlett@gmail.com>
2014-08-09 12:25:46 +0100

    Update live import script

--- db/import_live.sh
+++ db/import_live.sh
@@ -1,3 +1,4 @@
 heroku pgbackups:capture --expire -a opensit
 curl -o latest.dump `heroku pgbackups:url -a opensit`
-pg_restore --verbose --clean --no-acl --no-owner -h localhost -U opensit -d opensit_development latest.dump
+pg_restore --verbose --clean --no-acl --no-owner -h localhost -U postgres -d opensit_development latest.dump
+rm latest.dump
